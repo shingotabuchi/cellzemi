e4b097a3a92115c22e75a44c92c7fbfdcfea8d0e
Shingo Tabuchi <63262819+unchiburi@users.noreply.github.com>
2023-12-12 04:16:04 +0900

    sand

---
+++ Assets/Scripts/Sand.cs
@@ -0,0 +1,127 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sand : MonoBehaviour
+{
+    public ComputeShader compute;
+    public Image plotImage;
+    Texture2D plotTexture;
+    RenderTexture renderTexture;
+    public int DIM;
+    public int loopCount;
+    public float bigProb;
+    public int spawnSizeMax;
+    public int spawnSizeMin;
+    public float n;
+
+    int Init, SpawnSand, Plot, Step;
+    ComputeBuffer sandBuffer, particleTypeBuffer, variableBuffer;
+    struct SandParticle
+    {
+        bool active;
+        int x;
+        int y;
+        int size;
+    };
+    private void OnDestroy()
+    {
+        sandBuffer.Dispose();
+        particleTypeBuffer.Dispose();
+        variableBuffer.Dispose();
+    }
+    void Start()
+    {
+        plotTexture = new Texture2D(DIM, DIM);
+        plotTexture.filterMode = FilterMode.Point;
+        plotImage.sprite = Sprite.Create(plotTexture, new Rect(0, 0, DIM, DIM), Vector2.zero);
+        renderTexture = new RenderTexture(DIM, DIM, 24);
+        renderTexture.enableRandomWrite = true;
+        compute.SetInt("DIM", DIM);
+        compute.SetInt("spawnSize", 1);
+
+        sandBuffer = new ComputeBuffer(DIM * DIM, Marshal.SizeOf(typeof(SandParticle)));
+        particleTypeBuffer = new ComputeBuffer(DIM * DIM, sizeof(int));
+        variableBuffer = new ComputeBuffer(10, sizeof(int));
+
+        Init = compute.FindKernel("Init");
+        compute.SetBuffer(Init, "variableBuffer", variableBuffer);
+        compute.SetBuffer(Init, "sandBuffer", sandBuffer);
+        compute.SetBuffer(Init, "particleTypeBuffer", particleTypeBuffer);
+
+        SpawnSand = compute.FindKernel("SpawnSand");
+        compute.SetBuffer(SpawnSand, "variableBuffer", variableBuffer);
+        compute.SetBuffer(SpawnSand, "sandBuffer", sandBuffer);
+        compute.SetBuffer(SpawnSand, "particleTypeBuffer", particleTypeBuffer);
+
+        Step = compute.FindKernel("Step");
+        compute.SetBuffer(Step, "sandBuffer", sandBuffer);
+        compute.SetBuffer(Step, "particleTypeBuffer", particleTypeBuffer);
+
+        Plot = compute.FindKernel("Plot");
+        compute.SetBuffer(Plot, "particleTypeBuffer", particleTypeBuffer);
+        compute.SetTexture(Plot, "renderTexture", renderTexture);
+
+        compute.Dispatch(Init, (DIM * DIM + 63) / 64, 1, 1);
+        compute.Dispatch(Plot, (DIM + 7) / 8, (DIM + 7) / 8, 1);
+        RenderTexture.active = renderTexture;
+        plotTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        plotTexture.Apply();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var touchPoint = MousePositionToCanvasPosition((Vector2)Input.mousePosition);
+            if (PosOnCanvas(touchPoint))
+            {
+                int spawnX = (int)(touchPoint.x / 1080f * (float)DIM) + (Random.Range(-5, 6));
+                int spawnY = (int)(touchPoint.y / 1080f * (float)DIM) + (Random.Range(-5, 6));
+                // print(x.ToString() + " " + y.ToString());
+                if (spawnX < 0 || spawnX >= DIM || spawnY < 0 || spawnY >= DIM) return;
+                compute.SetInt("spawnX", spawnX);
+                compute.SetInt("spawnY", spawnY);
+
+                // int x1 = spawnSizeMax;
+                // int x0 = spawnSizeMin;
+                // float y = Random.Range(0f, 1f);
+                // float x = x1 - Mathf.Pow((Mathf.Pow(x1, (n + 1)) - Mathf.Pow(x0, (n + 1))) * y + Mathf.Pow(x0, (n + 1)), (1 / (n + 1)));
+                // int spawnSize = (int)(Mathf.Max(1f, x + 1f));
+
+                // if (spawnSize != 1) print(spawnSize);
+                // compute.SetInt("spawnSize", spawnSize);
+                // // if (Random.Range(0f, 1f) < bigProb)
+                // //     {
+                // //         compute.SetInt("spawnSize", 2);
+                // //     }
+                // //     else compute.SetInt("spawnSize", 1);
+                compute.Dispatch(SpawnSand, 1, 1, 1);
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        for (int kk = 0; kk < loopCount; kk++)
+        {
+            int rndSeed = Random.Range(0, int.MaxValue);
+            compute.SetInt("rndSeed", rndSeed);
+            compute.Dispatch(Step, (DIM * DIM + 63) / 64, 1, 1);
+        }
+        compute.Dispatch(Plot, (DIM + 7) / 8, (DIM + 7) / 8, 1);
+        RenderTexture.active = renderTexture;
+        plotTexture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        plotTexture.Apply();
+    }
+    Vector2 MousePositionToCanvasPosition(Vector2 mousePosition)
+    {
+        return ((Vector2)mousePosition - plotImage.GetComponent<RectTransform>().anchoredPosition);
+    }
+    bool PosOnCanvas(Vector2 pos)
+    {
+        if (pos.x < 0) return false;
+        if (pos.x >= 1080) return false;
+        if (pos.y < 0) return false;
+        if (pos.y >= 1080) return false;
+        return true;
+    }
+}
